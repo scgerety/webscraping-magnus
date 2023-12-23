b784c75d296f7bc001ff79b9fc354fbb989adf44
scgerety <71694412+scgerety@users.noreply.github.com>
2023-12-23 13:45:08 +0000

    modified:   chesscom.py

--- chesscom.py
+++ chesscom.py
@@ -17,6 +17,9 @@ class ChesscomSpider(scrapy.Spider):
                 break
         tablehead = response.xpath('//table')[0].xpath('thead')
         headers = [th.extract() for th in tablehead.xpath('tr').xpath('th/text()')]
+        table = "CREATE TABLE magnus ([Player1] TEXT, [Rating1] TEXT, [Player2] TEXT, [Rating2] TEXT, [OpeningMoves] TEXT, [OpeningName] TEXT, [Result] TEXT, [NumMoves] TEXT, [Year] TEXT)"
+        connection = sqlite3.connect("chesscom.db")
+
         tbody = response.xpath('//table')[0].xpath('//tbody')[0]
         for row in tbody.xpath('//tr'):
             player1, rating1, player2, rating2 = [cell.xpath('a/div/span/text()').extract() for cell in row.xpath('td')]
