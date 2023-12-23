88439ad5da13fbd5078add8f1e5d55e04fa9e44f
scgerety <71694412+scgerety@users.noreply.github.com>
2023-12-23 02:00:00 +0000

    modified:   chesscom.py 	renamed:    magnus-download -> magnus-download.html

--- chesscom.py
+++ chesscom.py
@@ -3,7 +3,7 @@ import os
 import sqlite3
 
 current_directory = os.path.dirname(__file__)
-url = os.path.join(current_directory, "magnus-download")
+url = os.path.join(current_directory, "magnus-download.html")
 
 class ChesscomSpider(scrapy.Spider):
     name = "chesscom"
@@ -11,4 +11,10 @@ class ChesscomSpider(scrapy.Spider):
     start_urls = [f"file://{url}"]
 
     def parse(self, response):
-        pass
+        for child in response.xpath('//table'):
+            if len(child.xpath('tr')) >=10:
+                table = child
+                break
+        tablehead = response.xpath('//table')[0].xpath('thead')
+        headers = [th.extract() for th in tablehead.xpath('tr').xpath('th/text()')]
+
